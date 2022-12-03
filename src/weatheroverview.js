5d71978eb7f1de7324e101db439fc0eaedea9524
Mark McNaughton <markusmack@hotmail.com>
2022-12-03 16:17:39 +0000

    added luxon date formatting

--- src/weatheroverview.js
+++ src/weatheroverview.js
@@ -1,4 +1,5 @@
 import { subscribe } from "./pubsub.js";
+import { DateTime } from "./libs/luxon.js";
 
 export function initOverview() {};
 
@@ -6,11 +7,19 @@ subscribe('weatherFetched', (data) => {
   updateOverview(data);
 });
 
+// Format a new date
+const currentDate = DateTime.now();
+var newFormat = {...DateTime.DATE_HUGE, weekday: 'long' };
+
 // Cache the DOM
 const weatherType = document.querySelector('#weather-type');
 const location = document.querySelector('#location');
+const date = document.querySelector('#date');
+const time = document.querySelector('#time');
 
 function updateOverview(data) {
   weatherType.textContent = data.weather[0].main;
   location.textContent = data.name;
+  date.textContent = currentDate.toLocaleString(newFormat);
+  time.textContent = currentDate.toLocaleString(DateTime.TIME_SIMPLE);
 }
